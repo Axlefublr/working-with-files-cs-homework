f29a2d7599e92f5efebc341af8af2c5c2a7b49b4
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2022-12-11 10:42:04 +0800

    Added Person, Deserializer, IOHelper

---
+++ Task4/Deserializer.cs
@@ -0,0 +1,4 @@
+namespace Task4;
+internal class Deserializer {
+   
+}
---
+++ Task4/Person.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Task4;
+
+internal class Person {
+   internal string Name;
+   internal string Group;
+   internal DateTime DateOfBirth;
+}
